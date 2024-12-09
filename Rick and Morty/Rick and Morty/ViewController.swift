7ca56bc27d3a2c511d1d977aa6ca2e5871372d23
Guilherme Ferreira Lenzolari <guilhermeferreiralenzolari@MacBook-Pro-de-Guilherme-2.local>
2024-12-09 17:30:58 -0300

    starts empty view code project

---
+++ Rick and Morty/Rick and Morty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rick and Morty
+//
+//  Created by Guilherme Ferreira Lenzolari on 09/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
---
+++ Rick and Morty/Rick and MortyUITests/Rick_and_MortyUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Rick_and_MortyUITestsLaunchTests.swift
+//  Rick and MortyUITests
+//
+//  Created by Guilherme Ferreira Lenzolari on 09/12/24.
+//
+
+import XCTest
+
+final class Rick_and_MortyUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
